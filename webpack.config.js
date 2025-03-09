9d651ea8e510377218148cdd9d6697128808d881
meisullum <meisullum@gmail.com>
2025-03-09 16:32:13 -0500

    Added options page + CSS loaders

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
     devtool: 'cheap-module-source-map',
     entry: {
         popup: path.resolve('src/popup/popup.tsx'),
+        options: path.resolve('src/options/options.tsx'),
     },
     module: {
         rules: [
@@ -20,23 +21,44 @@ module.exports = {
                 use: 'ts-loader',
                 test: /\.tsx?$/,
                 exclude: /node_modules/,
-            }
-        ]
+            },
+            {
+                use: ['style-loader', 'css-loader'],
+                test: /\.css$/i,
+            },
+            {
+                type: 'asset/resource',
+                test: /\.(jpg|jpeg|png|woff|woff2|eot|tt|svg)$/,
+            },
+        ],
     },
     plugins: [
         new CopyPlugin({
             patterns: [
                 {
-                    from: path.resolve('src/manifest.json'),
+                    from: path.resolve('src/static'),
                     to: path.resolve('dist')
                 }
             ]
         }),
-        new HtmlPlugin({
-            title: 'React Extension',
-            filename: 'popup.html',
-            chunks: ['popup']
-        })
+        ...getHtmlPlugins(
+            [
+                'popup',
+                'options'
+            ]  
+        ),
+        // new HtmlPlugin(
+        //     {
+        //         title: 'React Extension',
+        //         filename: 'popup.html',
+        //         chunks: ['popup']
+        //     },
+            // {
+            //     title: 'Options Extension',
+            //     filename: 'options.html',
+            //     chunks: ['options']
+            // },
+        // )
     ],
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
@@ -47,5 +69,19 @@ module.exports = {
         filename: '[name].js',
         path: path.resolve('dist')
     },    
+    optimization: {
+        // allows chunks to share modules
+        splitChunks: {
+            chunks: 'all',
+        },
+    }
 
+}
+
+function getHtmlPlugins(chunks) {
+    return chunks.map(chunk => new HtmlPlugin({
+        title: 'React Extension',
+        filename: `${chunk}.html`,
+        chunks: [chunk]
+    }));
 }
